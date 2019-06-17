6cd8bf3811376c6e3f588aac4da6ded29b077202
Carlosf96 <carlos.f.roque96@gmail.com>
2019-06-17 16:40:20 -0500

    Initial design

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+(function($) {
+
+  // Init Wow
+  wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+  });
+  wow.init();
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  // Navigation scrolls
+  $('.navbar-nav li a').bind('click', function(event) {
+    $('.navbar-nav li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
+
+  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Carlos Franco", "Designer", "Developer", "Freelancer", "Carlos Franco"],
+      typeSpeed: 1,
+      loop: false,
+    });
+  });
+
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+
+  });
+
+})(jQuery);
