83934c408d643a0ba0963bf6a11ca3dcb95a9106
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-19 16:41:51 -0500

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
+const path = require('path');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
-    }
-  };
+  entry: ['./client/src/Index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules|packages/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader','sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    contentBase: './client/dist',
+    host: 'localhost',
+    historyApiFallback: true,
+    // respond to 404s with index.html
+    inline: true,
+  },
+};
